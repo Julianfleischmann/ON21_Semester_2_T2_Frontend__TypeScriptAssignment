f20825d6ac8db8cf81ec6715c6cd80e13cfe8da0
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-04-24 18:11:07 +0200

    Added the dom utils and exporting them

---
+++ src/calculatorDomUtils.ts
@@ -0,0 +1,5 @@
+export const getConsumptionInLiter = document.getElementById('consumptionInLiter') as HTMLInputElement;
+export const getDistanceInKm = document.getElementById('distanceInKm') as HTMLInputElement;
+export const getCheckTwoTimes = document.getElementById('checkTwoTimes') as HTMLInputElement;
+export const getPriceInEuro = document.getElementById('priceInEuro') as HTMLInputElement;
+export const getPriceOpnv = document.getElementById('priceOpnv') as HTMLInputElement;
