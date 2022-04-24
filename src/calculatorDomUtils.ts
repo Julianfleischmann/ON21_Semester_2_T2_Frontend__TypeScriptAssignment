2e588621cb73942900ed9c99a670a5c258ad183d
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-04-24 18:55:53 +0200

    Added update Function

--- src/calculatorDomUtils.ts
+++ src/calculatorDomUtils.ts
@@ -3,3 +3,8 @@ export const getDistanceInKm = document.getElementById('distanceInKm') as HTMLIn
 export const getCheckTwoTimes = document.getElementById('checkTwoTimes') as HTMLInputElement;
 export const getPriceInEuro = document.getElementById('priceInEuro') as HTMLInputElement;
 export const getPriceOpnv = document.getElementById('priceOpnv') as HTMLInputElement;
+export const getCostCar = document.getElementById('costCar');
+
+export function updateCostCar() {
+    getCostCar!.innerHTML = "hallo";
+}
