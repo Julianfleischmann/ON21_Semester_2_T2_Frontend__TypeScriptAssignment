dc1f0e3bc7ec42ff5bb67a96cfac0808e088a3ba
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-04-26 21:28:03 +0200

    removed unnecessary code and files

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,5 @@
 //THIS IS THE ENTRY FILE - WRITE YOUR MAIN LOGIC HERE!
 
-// import { helloWorld, Beispiel } from "./myModule";
-// import { alertMe } from "./myOtherModule";
-
-// console.log(helloWorld);
-// customElements.define("my-beispiel", Beispiel);
-
-// alertMe();
-
-// const myInputValue = document.querySelector<HTMLInputElement>("#myInput");
-
-// const myInputValueAlternate = document.querySelector(
-//   "#myInput"
-// ) as HTMLInputElement;
-
-// document
-//   .querySelector<HTMLInputElement>("#myInput")
-//   ?.addEventListener("focus", doSmth);
-
-// function doSmth(e: UIEvent) {
-//   const val = e.target as HTMLInputElement;
-//   console.log(e, val.value);
-// }
-
 import { getConsumptionInLiter, getCostCarResult, getDistanceInKm, getCarPriceInEuro, getPriceOpnv, getCostOpnvResult, getCarCheckTwoTimes } from './calculatorDomUtils';
 import { calcCarOpnv } from './calculatorLogic';
 
