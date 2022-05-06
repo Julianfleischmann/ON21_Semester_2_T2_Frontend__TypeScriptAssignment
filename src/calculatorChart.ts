7fb3c92123dcb9ff10051571367f8b59f9a9fe93
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-05-07 01:20:33 +0200

    Added some comments, tried to add a custom interface didn't work because of chart.js :(

--- src/calculatorChart.ts
+++ src/calculatorChart.ts
@@ -7,7 +7,7 @@ export const priceChart = new Chart (getChart, {
             labels: ["Kosten Auto", "Kosten ÖPNV"], // todo: Variable?
             datasets: [{
                 label: 'Vergleich der Kosten',
-                data: [], // todo: replace fakedata
+                data: [],
                 backgroundColor: [
                     '#3a9691',
                     '#1e3932'
@@ -22,16 +22,28 @@ export const priceChart = new Chart (getChart, {
     }
 )
 
+// Setzt das Chart, den Array zurück (sonst würden die Werte immer addiert werden)
 export function resetChart() {
-    priceChart.data.datasets.forEach((element: any) => {
+    priceChart.data.datasets.forEach((element: any) => { // Todo: Any prüfen
         element.data = [];
     });
 }
 
-export function updatePriceChart (carResult?: number, opnvResult?: number) {
+export function updatePriceChart (carResult: number, opnvResult: number) {
     resetChart();
-    priceChart.data.datasets.forEach((element: any) => { // Type any überdenken!
+    priceChart.data.datasets.forEach((element: any) => { // Todo: Any prüfen
         element.data.push(carResult, opnvResult);
     });
     priceChart.update();
 }
+
+
+
+// aktuell nicht in Benutzung
+// interface datasetElement {
+//     label: string,
+//     data: Array<number>,
+//     // backgroundColor: Array<Color>, // https://www.chartjs.org/docs/latest/api/#color
+//     // borderColor: Array<Color>, // https://www.chartjs.org/docs/latest/api/#color
+//     // borderWidth: number | { top?: number, right?: number, bottom?: number, left?: number }; // https://www.chartjs.org/docs/latest/api/interfaces/BarOptions.html#borderwidth
+// }
