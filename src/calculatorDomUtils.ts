4d6348877fe85b1c71ae07b2b39cb89ad02c9493
Julian Fleischmann <julian.fleischmann@mr-pixel.de>
2022-05-07 01:34:52 +0200

    Added doctype documentation to Dom Utils

--- src/calculatorDomUtils.ts
+++ src/calculatorDomUtils.ts
@@ -1,17 +1,49 @@
-// Car variables
-// input fields
+// Start Variables for Car
+
+/**
+ * Car variable
+ * input field – consumption
+ */
 export const getConsumptionInLiter = document.getElementById('consumptionInLiter') as HTMLInputElement;
+/**
+ * Car variable
+ * input field – distance
+ */
 export const getDistanceInKm = document.getElementById('distanceInKm') as HTMLInputElement;
+/**
+ * Car variables
+ * input field – boolean for way back
+ */
 export const getCarCheckTwoTimes = document.getElementById('checkTwoTimes') as HTMLInputElement;
+/**
+ * Car variable
+ * input field – price
+ */
 export const getCarPriceInEuro = document.getElementById('priceInEuro') as HTMLInputElement;
-// Result
+/**
+ * Car variable
+ * result output – output the result into DOM
+ */
 export const getCostCarResult = document.getElementById('costCarResult') as HTMLElement;
 
-// ÖPNV variables
-// input fields
+
+// Start Variables for ÖPNV
+
+/**
+ * ÖPNV variables
+ * input fields
+ */
 export const getPriceOpnv = document.getElementById('priceOpnv') as HTMLInputElement;
-// result
+/**
+ * ÖPNV variables
+ * result output
+ */
 export const getCostOpnvResult = document.getElementById('costOpnvResult') as HTMLElement;
 
-// Chart variables
+
+// Start Canvas Element
+
+/** 
+ * Get ID with chart Canvas for Chart variables – output the result into DOM
+ */
 export const getChart = document.getElementById('priceChart') as HTMLCanvasElement;
